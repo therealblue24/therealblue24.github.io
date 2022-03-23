5aa420c59585b4ffbcf230900bdbd299bea25e97
TheRealBlue24 <99767624+therealblue24@users.noreply.github.com>
2022-03-23 18:56:53 -0400

    W.I.P.

---
+++ apt/package2get/pkg2get.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+int main(int argc, char *argv[]) {
+    if(argc == 3) {
+        
+    } else {
+        printf("Usage: pkg2get <directory> <storedirectory>");
+        return 1;
+    }
+}
